b1c4bcf0a192c5c8f6dc1bfa7ebb8172ffe2e6f0
xx8o3ii <fanchan0627@gmail.com>
2024-08-28 21:58:28 +0800

    header&sidebar未完成

--- quotation_system/src/index.js
+++ quotation_system/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './Header';
 import reportWebVitals from './reportWebVitals';
+import Sidebar from './Sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
     <Header />
+    <Sidebar />
   </React.StrictMode>
 );
 
